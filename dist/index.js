2d3d30ac4eb25d4bc4c2478672927c5191ad72a2
KamilHs <kamilsalimli1940@gmail.com>
2021-01-28 23:23:40 +0400

    cors and body-parser added

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-    res.send("Hello world!");
+    res.send("Hello world twice!");
 });
+
 app.listen(process.env.PORT || 5555);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import express = require("express");
+import cors = require("cors");
+import bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(cors({
+    origin: process.env.ALLOWED_ORIGIN || "http://localhost:3000",
+    credentials: true
+}))
+app.use(bodyParser.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send("Hello world!")
